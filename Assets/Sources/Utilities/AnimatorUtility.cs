e793073853eefa3ea9da59b4681886aadc223e32
Uchiko <memememomo@gmail.com>
2015-12-11 17:03:49 +0900

    Add AnimatorUtility

---
+++ Assets/Sources/Utilities/AnimatorUtility.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorUtility : MonoBehaviourUtility
+{
+	Animator _animator;
+
+	public Animator Animator {
+		get {
+			if (_animator == null)
+				_animator = GetComponent<Animator> ();
+			return _animator;
+		}
+	}
+
+	public bool IsFinishAnimation {
+		get;
+		private set;
+	}
+
+	public int FinishAnimationCounter {
+		get;
+		private set;
+	}
+
+	public void SetAnimation(int state, float speed = -1.0f)
+	{
+		IsFinishAnimation = false;
+		FinishAnimationCounter = 0;
+		if (speed < 0f) {
+			Animator.speed = speed;
+		}
+		Animator.SetInteger("state", state);
+	}
+
+	public int GetAnimation()
+	{
+		return Animator.GetInteger ("state");
+	}
+
+	public void OnFinishAnimation()
+	{
+		FinishAnimationCounter++;
+		IsFinishAnimation = true;
+	}
+}
