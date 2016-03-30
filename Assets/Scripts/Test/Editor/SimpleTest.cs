380d4ed231c136a9626847fff3acd59d94c56240
Uchiko <memememomo@gmail.com>
2016-03-30 21:52:23 +0900

    Add Test

---
+++ Assets/Scripts/Test/Editor/SimpleTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class SimpleTest {
+
+    [Test]
+    public void EditorTest()
+    {
+        //Arrange
+        var gameObject = new GameObject();
+
+        //Act
+        //Try to rename the GameObject
+        var newGameObjectName = "My game object";
+        gameObject.name = newGameObjectName;
+
+        //Assert
+        //The object has a new name
+        Assert.AreEqual(newGameObjectName, gameObject.name);
+    }
+
+	[Test]
+	public void StringExtensionTest()
+	{
+		Assert.AreEqual("a b", "{0} {1}".Fmt("a", "b"));
+		Assert.True("".IsNullOrWhiteSpace());
+		Assert.True(" ".IsNullOrWhiteSpace());
+		Assert.False(" a ".IsNullOrWhiteSpace());
+	}
+}
